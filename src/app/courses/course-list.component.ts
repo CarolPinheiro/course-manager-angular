52a1d6d806077ae015a85538b355ced4a7bb069f
carolpinheiro <ommasshi@gmail.com>
2021-05-07 09:37:52 -0300

    Injeção de dependências configurada

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course-service";
 
 @Component({
   selector: 'app-course-list',
@@ -8,28 +9,11 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) {
+
+  }
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: "Aprendendo Angular Forms",
-        imageUrl: "/assets/images/forms.png",
-        price: 99.99,
-        code: "xpt099",
-        duration: 200,
-        rating: 5,
-        releaseDate: "December, 2, 2019"
-      },
-      {
-        id: 2,
-        name: "Aprendendo Angular HTTP",
-        imageUrl: "/assets/images/http.png",
-        price: 44.59,
-        code: "xpt359",
-        duration: 150,
-        rating: 4.5,
-        releaseDate: "January, 9, 2021"
-      }
-    ]
+    this.courses = this.courseService.retrieveAll()
+
   }
 }
